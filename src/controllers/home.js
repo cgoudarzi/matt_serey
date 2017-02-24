5e283d285a42efdfc87b653a563b96136c4e591a
Cyrus Goudarzi <cgoudarzi@gmail.com>
2017-02-24 15:08:41 -0500

    sendgrid implemented on form

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,11 +1,66 @@
-var express   = require('express');
-    Home      = express.Router();
-    mongoose  = require('mongoose');
-    fs        = require('fs')
+var express     = require('express');
+    Home        = express.Router();
+    Submission  = require('../models/submission');
+    mongoose    = require('mongoose');
+    helper      = require('sendgrid').mail;
+    fs          = require('fs')
+
+Home.route('/submissions')
+  .get(function(req, res, next) {
+    Submission.find({}, function(err, submissions) {
+      var list = submissions;
+      res.send(list)
+    })
+  });
+
+Home.route('/contact') 
+  .get(function(req, res, next) {
+    res.render('contact')
+  })
+
+  .post(function(req, res, next) {
+    console.log(req.body);
+    console.log(req.body.email);
+    console.log(req.body.subject);
+    console.log(req.body.message);
+    Submission.create({
+      name: req.body.name,
+      email: req.body.email,
+      subject: req.body.subject,
+      message: req.body.message
+    },
+    function (error, submission) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(submission);
+        res.redirect('/');
+      }
+    });
+
+    from_email = new helper.Email(req.body.email);
+    to_email   = new helper.Email(process.env.EMAIL);
+    subject    =     req.body.subject;
+    content    = new helper.Content("text/plain", "Name: " + req.body.name + "\n\nMessage: " + req.body.message);
+    mail       = new helper.Mail(from_email, subject, to_email, content);
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+      console.log(response.headers);
+    })
+  })
 
 Home.route('/')
   .get(function(req, res, next) {
-    res.render('Home')
+    res.render('home')
   })
 
 module.exports = Home;
