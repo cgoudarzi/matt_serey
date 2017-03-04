99b97521bc1bca8f28d6b2db29e1e31fd75cc5d0
Cyrus Goudarzi <cgoudarzi@gmail.com>
2017-03-04 01:06:31 -0500

    link js and state size dimensions

---
+++ src/public/js/main.js
@@ -0,0 +1,17 @@
+console.log(window.location.href);
+
+function linkColor (link) {
+    if (link === 'http://localhost:8000/') {
+    document.getElementById('home').style.color="#C91AC4";
+  } else if (link === 'http://localhost:8000/reel-1' || link === 'http://localhost:8000/cv') {
+    document.getElementById('work').style.color="#C91AC4";
+  } else if (link ==='http://localhost:8000/contact') {
+    document.getElementById('contact').style.color="#C91AC4";
+  } else if (link ==='http://localhost:8000/about-me') {
+    document.getElementById('about').style.color="#C91AC4";
+  } else {
+    console.log('failure');
+  }
+}
+
+linkColor(window.location.href);
